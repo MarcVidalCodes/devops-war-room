780c4a4495dab2815ab6e8b5ac8cdc7a3b5eec4a
MarcVidalCodes <marcgavvid@gmail.com>
2025-12-04 21:02:20 -0500

    Chore: Update test config and traffic script for remediation demo

--- examples/timed_traffic.sh
+++ examples/timed_traffic.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Running traffic for 90 seconds..."
-end=$((SECONDS+90))
+echo "Running traffic for 300 seconds..."
+end=$((SECONDS+300))
 
 while [ $SECONDS -lt $end ]; do
     echo "[$(date +%H:%M:%S)] Sending burst..."
-    for i in {1..20}; do curl -s http://localhost:5001/api/v1/products > /dev/null & done
+    for i in {1..5}; do curl -s http://localhost:5001/api/v1/products > /dev/null & done
     wait
-    sleep 2
+    sleep 1
 done
 echo "Done."
